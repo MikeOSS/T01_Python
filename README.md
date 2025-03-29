0cd2e5c55fa5e3e8b72a4c2bedbb31dd514db9df
mikeOSS <miguel.costacurta3@gmail.com>
2025-03-29 11:37:07 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Jogo desenvolvido em Python para um trabalho requerido pela diciplina de lógica de programação.
+Nesse game faremos a geração de um número secreto aleatório de quatro dígitos, em que dentro de 10 tentativas o usuário deverá adivinhar
+os algarimos, de modo que a partir da 5 tentativa o jogo deverá fornecer dicas para que o usuário consiga acertar! 
---
+++ T01_Python.py
@@ -0,0 +1,18 @@
+import random
+
+# gerando número aleatório e separando por partes:
+# numero_secreto = random.randint(1000, 9999)
+
+# primeiro = numero_secreto //1000
+# segundo= (numero_secreto % 1000)//100
+# terceiro= (numero_secreto % 1000) // 10
+# quarto = (numero_secreto % 1000) % 10
+
+# print(numero_secreto)
+# print(primeiro)
+# print(segundo)
+# print(terceiro)
+# print(quarto)
+
+#
+        
