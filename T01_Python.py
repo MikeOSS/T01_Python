1922a595bd26840cd481ecbe19bf0cdbc1aab05c
maxthba <maxthomaz2006@gmail.com>
2025-03-31 14:24:30 -0300

    commit 2.0

--- T01_Python.py
+++ T01_Python.py
@@ -48,19 +48,19 @@ while (cont > 0):
     t_unidade = tentativa % 10 
 
     if milhar == t_milhar:
-            num1=milhar    
-    if cent == t_centena:
+        num1=milhar    
+        if cent == t_centena:
             num2 == cent
-    if dez == t_dezena:
-            num3 == dez
-    if unidade == t_unidade:
-            num4 == unidade
-
-    print(f"{num1} {num2} {num3} {num4}")
-    cont -= 1
+            if dez == t_dezena:
+                num3 == dez
+                if unidade == t_unidade:
+                    num4 == unidade
+                    print(f"{num1} {num2} {num3} {num4}")
+                    cont -= 1
     
 
-    if(num1==milhar and num2==cent and num3 == dez and num4 == unidade): # Tela de vitória direto1
+    else:
+        if(num1==milhar and num2==cent and num3 == dez and num4 == unidade): # Tela de vitória direto1
         cont = 0
         print('\n' * 5)
         print(' ' * 15 + "P Á R A B E N S ! ! !")
