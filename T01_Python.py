3a13156dfed0b16c879c4be47c06f35fb4960e90
mikeOSS <miguel.costacurta3@gmail.com>
2025-04-12 16:57:53 -0300

    versão final 3.0

--- T01_Python.py
+++ T01_Python.py
@@ -1,14 +1,12 @@
 import random
 
-# gerando número aleatório
 numero_ficticio = random.randint(1000, 9999)
-# divisão dos números:
+
 milhar = numero_ficticio // 1000
 cent = (numero_ficticio % 1000) // 100
 dez = (numero_ficticio % 100) // 10
 unidade = (numero_ficticio) % 10
 
-# Tela inicial:
 print("\n" * 5)
 print(" " * 10 + "=> Bem vindo Ao Jogo SECRETO!!! <=")
 print("\n" * 2)
@@ -17,15 +15,11 @@ print(
 print(" " * 20 + "<<< Tecle Algo >>>")
 input()
 num_de_jogadas = 1
-# Tela principal
 
-
-# contadores
 cont = 10
 cont_tentativas = 0
 cont_algarismos = 0
 
-# checando os algarismos certos
 m_check = 0
 c_check = 0
 d_check = 0
@@ -33,9 +27,7 @@ u_check = 0
 
 while num_de_jogadas > 0:
     while (cont > 0):
-        # contando as tentativas:
         tentativa = int(input(f'digite sua tentativa de código: '))
-        # Tentativa de fazer o programa não reconhecer numeros fora do escopo e letras digitadas!
 
         while (tentativa < 1000 or tentativa > 9999):
             print('\t\t\t A T E N Ç Ã O!!\n')
@@ -45,14 +37,12 @@ while num_de_jogadas > 0:
             tentativa = int(input(f'digite sua tentativa de código: '))
 
         cont_tentativas += 1
-        # dividindo o número que usuário digitou
+
         t_milhar = tentativa // 1000
         t_centena = (tentativa % 1000) // 100
         t_dezena = (tentativa % 100) // 10
         t_unidade = tentativa % 10
-        # faz o numero aparecer na tela quando acertado e conta os algarismos certos
 
-        # mudando o valor da varável de checagem pra um caso os algarismos sejam iguais. de modo a sempre printar o algarismo certo quando for 1, mesmo quano o User digite errado dps de acertar
         if milhar == t_milhar:
             m_check = 1
         if cent == t_centena:
@@ -85,11 +75,10 @@ while num_de_jogadas > 0:
             cont_algarismos += 1
         else:
             print("_")
-        # contador de numeros faltantes
+    
         cont_falt = 4 - cont_algarismos
         cont -= 1
 
-        # quando acertou o numero
         if (t_milhar == milhar and t_centena == cent and t_dezena == dez and t_unidade == unidade):
             cont = 0
             print('\n' * 5)
@@ -101,7 +90,6 @@ while num_de_jogadas > 0:
             print(' ' * 50 + "<<< tecle algo >>>")
             input()
         else:
-            # verificando se o usuário acertou algum algarismo
             if (cont_algarismos > 0):
                 print(f'\nVocê acertou {cont_algarismos} algarismos!\n')
                 cont_algarismos = 0
@@ -111,7 +99,6 @@ while num_de_jogadas > 0:
             print(
                 f"faltam {cont_falt} algarismos e {cont} tentativas\n")
         elif cont <= 5:
-            # resuldado com dicas
 
             if m_check != 1:
                 if milhar < 5:
@@ -145,7 +132,7 @@ while num_de_jogadas > 0:
         print(f"acabaram as tentativas\n")
         print('Você não consguiu acertar!\n')
         print(f"O código era {numero_ficticio}\n")
-# vendo se o jogador quer jogar mais uma vez dps de ter acabado as tentativas ou ter ganhado o jogo
+        
     if (cont < 1):
         more1 = int(input("jogar mais uma vez?? 1=SIM E 2=NÃO \n==>"))
         if (more1 == 1):
